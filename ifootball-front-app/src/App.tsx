7c69ad51e8b742b517e3a00b755f97c5c4403cab
Nícolas Augusto Köhler Arend <107890287+NicolasIFRS@users.noreply.github.com>
2023-06-14 09:10:23 -0300

    front-Homepage

--- ifootball-front-app/src/App.tsx
+++ ifootball-front-app/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.scss';
+import Homepage from './pages/Homepage';
 
 function App() {
   return (
     <div className="App">
-      
+      <Homepage/>
     </div>
   );
 }
---
+++ ifootball-front-app/src/pages/Homepage/index.tsx
@@ -0,0 +1,13 @@
+import './style.scss';
+
+const Homepage = () => {
+    
+
+
+    return(
+        <div><h1>( ͡° ͜ʖ ͡°) 🍆   👉👌</h1></div>
+    )
+
+}
+
+export default Homepage;
